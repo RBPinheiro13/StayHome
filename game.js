d0935f0e7b2663c494292a39b93d3c8fd4c1872b
Rodrigo <rodrigoborba1997@gmail.com>
2020-04-26 15:55:50 +0200

    Offset of canvas

--- game.js
+++ game.js
@@ -350,8 +350,8 @@ controller = {
       for (index1 = target_touches.length - 1; index1 > -1; -- index1) {
 
         touch = target_touches[index1];
-        controller.touch_X = touch.clientX;
-        controller.touch_Y = touch.clientY;
+        controller.touch_X = touch.clientX - display.bounding_rectangle.left;
+        controller.touch_Y = touch.clientY - display.bounding_rectangle.top;
 
         // make sure the touch coordinates are adjusted for both the canvas offset and the scale ratio of the buffer and output canvases:
         if (button.containsPoint((touch.clientX - display.bounding_rectangle.left) * display.buffer_output_ratio, (touch.clientY - display.bounding_rectangle.top) * display.buffer_output_ratio)) {
